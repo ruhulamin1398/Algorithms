5f7c62605185c85de332cc8317620e2013a14e6d
ruhulaminrul <ruhul.ok@gmail.com>
2019-08-28 23:43:44 +0600

    using deque

---
+++ data structure/sliding range minimum query.cpp
@@ -0,0 +1,36 @@
+/*
+The smallest number in every segment
+
+log n
+
+output  :
+10 12 3 3 2 2 2 44 55 22 11 11 11 5
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int arr[]={10,12,21,3,44,7,2,44,55,66,44,22,11,41,25,5};
+
+    int len= sizeof(arr)/sizeof(arr[0]);
+    int segment=3;
+    vector<int> result;
+    vector<int > :: iterator it;
+    deque<int > d;
+    d.push_back(arr[0]);
+
+    for( int i =1 ; i<len ; i++){
+            if(d.back()>arr[i])
+                d.pop_back();
+            d.push_back(arr[i]);
+            if(i<(segment-1))
+                continue;
+            result.push_back(d.front());
+            if(d.front()==arr[i-segment+1])
+                d.pop_front();
+    }
+    for( it= result.begin() ; it!=result.end() ; it++)
+        cout<<*it<<" ";
+    return 0 ;
+}
